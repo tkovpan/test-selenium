839d6d231efcae956c58de3eb453fc8b66366194
tatiana.kovpan <tkovpan@gmail.com>
2017-06-27 09:36:51 +0300

    task 7 updated

--- src/test/java/Task7.java
+++ src/test/java/Task7.java
@@ -47,38 +47,40 @@ public class Task7 {
 
             driver.findElement(By.xpath("//*[@id=\"box-popular-products\"]/div/div"));
             AllDucks.get(i).click();
-            if (isElementPresent(By.xpath("//*[@id=\"box-product\"]/div[1]/div[3]/div/div[4]/form/div[1]/select"))){
+            if (isElementPresent(By.xpath("//*[@id=\"box-product\"]/div[1]/div[3]/div/div[4]/form/div[1]/select"))) {
                 Select select = new Select(driver.findElement(By.xpath("//*[@id=\"box-product\"]/div[1]/div[3]/div/div[4]/form/div[1]/select")));
                 select.selectByValue("Small");
-                }
+            }
             Thread.sleep(5);
             driver.findElement(By.name("add_cart_product")).click();
             Thread.sleep(5);
             driver.findElement(By.xpath("/html/body/div[2]/div/button")).click();
 
-            Thread.sleep(50);
+            Thread.sleep(5);
 
             wait.until(ExpectedConditions.textToBePresentInElementLocated(By.xpath("//*[@id=\"cart\"]/a/div/div[2]/span[1]"), String.valueOf(i + 1)));
-            Thread.sleep(50);
+            Thread.sleep(5);
         }
 
         driver.findElement(By.xpath("//*[@id=\"cart\"]/a/div")).click();
 
         for (; ; ) {
             wait.until(ExpectedConditions.elementToBeClickable(By.name("remove_cart_item"))).click();
-            Thread.sleep(50);
+            Thread.sleep(5);
 
             WebElement table = driver.findElement(By.xpath("//*[@id=\"box-checkout-cart\"]/div/table/tbody/tr[1]"));
             wait.until(ExpectedConditions.stalenessOf(table));
-            Thread.sleep(50);
+            Thread.sleep(5);
+
 
             if (!isElementPresent(By.xpath("//*[@id=\"box-checkout-cart\"]/h2"))) {
                 driver.findElement(By.cssSelector("a[href*='http://localhost/litecart/en/']")).click();
-
-
-            String items = driver.findElement(By.className("quantity")).getCssValue("item(s)-");
-            Assert.assertEquals("", items);
+                break;
             }
         }
-    }
-}
+
+                String items = driver.findElement(By.className("quantity")).getCssValue("item(s)-");
+                Assert.assertEquals("", items);
+            }
+        }
+
