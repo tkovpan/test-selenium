68c645855b1407c634cf35792b007489c3bf2b9c
tatiana.kovpan <tkovpan@gmail.com>
2017-07-04 11:07:08 +0300

    task10

--- src/test/java/Task10/Test/TestProducts.java
+++ src/test/java/Task10/Test/TestProducts.java
@@ -1,19 +1,19 @@
 package Task10.Test;
-
+import org.junit.Test;
 
 import Task10.Pages.AddProductPage;
 import Task10.Pages.RemoveProductsPage;
 
-public class Test extends TestBase {
+public class TestProducts extends TestBase {
 
     @Test
 
     public void addAndRemoveProducts(AddProductPage products) {
 
-       for (int i=0; i<3; i++) {
-           app.addProducts(products);
-       }
-   }
+        for (int i=0; i<3; i++) {
+            app.addProducts(products);
+        }
+    }
     public void addAndRemoveProducts(RemoveProductsPage products) {
         for(; ;){
             app.removeProducts(products);
