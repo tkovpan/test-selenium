260d53e64f59e9db4f68264f3edc9a9ff44d16f4
tatiana.kovpan <tkovpan@gmail.com>
2017-07-06 10:30:52 +0300

    added image

--- src/test/java/Task8.java
+++ src/test/java/Task8.java
@@ -1,3 +1,4 @@
+import com.sun.java.swing.plaf.windows.resources.windows;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.junit.After;
 import org.junit.Before;
@@ -6,20 +7,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Task8 {
     public WebDriver driver;
+    public WebDriverWait wait;
 
 
     @Before
     public void start() {
         ChromeDriverManager.getInstance().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, 4);
     }
+
+
 
     @Test
     public void login_admin() {
@@ -33,20 +42,30 @@ public class Task8 {
 
         List<WebElement> AllLinks = driver.findElements(By.xpath("//label/a/i"));
 
-       for(int i = 0; i < AllLinks.size(); i++){
+        for (int i = 0; i < AllLinks.size(); i++) {
+            String originalW = driver.getWindowHandle();
+            Set<String> existWs = driver.getWindowHandles();
             AllLinks = driver.findElements(By.xpath("//label/a/i"));
             AllLinks.get(i).click();
-           String parentLink = driver.getWindowHandle();
-            for (String winHandle : driver.getWindowHandles()) {
-                driver.switchTo().window(winHandle);
-            }
-           driver.close();
-           driver.switchTo().window(parentLink);
+            String newW = wait.until(anyWindowOtherThan(existWs));
+            driver.switchTo().window(newW);
+            driver.close();
+            driver.switchTo().window(originalW);
         }
     }
-
+    public ExpectedCondition<String> anyWindowOtherThan(Set<String> windows) {
+        return new ExpectedCondition<String>() {
+            public String apply(WebDriver input) {
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(windows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
+        };
+    }
     @After
     public void stop() {
         driver.quit();
     }
 }
+
+
